3b1e2f4cf0e7209b3c04be04fd349f9a9a0130e0
BigMushFrog23 <popov.jeremy.v@gmail.com>
2025-06-19 13:30:04 +0200

    added supabase stuff

--- register.php
+++ register.php
@@ -1,3 +1,10 @@
+<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/dist/supabase.min.js"></script>
+<script>
+  const supabaseUrl = 'YOUR_SUPABASE_URL';
+  const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY';
+  const supabase = supabase.createClient("https://ylaukgzoebsprbnwywfq.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlsYXVrZ3pvZWJzcHJibnd5d2ZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAzMzEzNTQsImV4cCI6MjA2NTkwNzM1NH0.9LXhRjOcS2fSU5UxceaXA1ET6zYc9NLqfhW-9qtT4tg");
+</script>
+
 <?php
 session_start();
 require 'db.php';
